eaa059c99eed3f4d60f1f447012b464db6f07e80
Forkeep <13126593533@163.com>
2020-03-29 02:29:32 +0800

    install chai

--- src/Button-Group.vue
+++ src/Button-Group.vue
@@ -5,7 +5,16 @@
 </template>
 
 <script lang="js">
-
+export default {
+  mounted() {
+    for (let node of this.$el.children) {
+      let name = node.nodeName.toLowerCase();
+      if (name !== 'button') {
+        console.warn(`button-group 中应该只有 g-button 但是你写的是 ${name}`)
+      }
+    }
+  }
+}
 
 </script>
 
@@ -15,17 +24,23 @@
     vertical-align: middle;
 
     .g-button {
+      &:not(:first-child) {
+        margin-left: -1px;
+      }
+
       border-radius: 0;
-      margin-left: -1px;
+
       &:first-child {
         border-top-left-radius: var(--border-radius);
         border-bottom-left-radius: var(--border-radius);
       }
+
       &:last-child {
         border-top-right-radius: var(--border-radius);
         border-bottom-right-radius: var(--border-radius);
       }
-      &:hover{
+
+      &:hover {
         z-index: 1;
         /*position: relative;*/
       }
