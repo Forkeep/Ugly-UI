e84fb2554b65b07b16c8afc612667e017b474698
Forkeep <13126593533@163.com>
2020-03-28 22:29:28 +0800

    组件化

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 一个自制UI框架
 ## 我不会设计，所以很丑陋
+## 安装
+使用本框架前请在css中开启border-box
+
+```css
+box-sizing : border-box;
+```
---
+++ src/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="g-button">点击我</button>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss">
+  body {
+    background-color: red;
+  }
+
+  .g-button {
+    border: 1px solid red;
+  }
+</style>
