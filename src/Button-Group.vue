270e9fd8f3423480ddcb135d4c61b99ecc188298
Forkeep <13126593533@163.com>
2020-03-29 02:08:32 +0800

    botton-group

---
+++ src/Button-Group.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="js">
+
+
+</script>
+
+<style lang="scss" scoped>
+  .button-group {
+    display: inline-flex;
+    vertical-align: middle;
+
+    .g-button {
+      border-radius: 0;
+      margin-left: -1px;
+      &:first-child {
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child {
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      &:hover{
+        z-index: 1;
+        /*position: relative;*/
+      }
+    }
+
+  }
+</style>
