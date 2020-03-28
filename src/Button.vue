eadef28f5d28190cffe27765536673cb8c0e4ff3
Forkeep <13126593533@163.com>
2020-03-29 01:07:49 +0800

    add icon & iconPosition

--- src/Button.vue
+++ src/Button.vue
@@ -1,17 +1,66 @@
 <template>
-  <button class="g-button">点击我</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href=" `#i-${icon}` "/>
+    </svg>
+    <div class="content">
+      <slot>--名称--</slot>
+
+    </div>
+  </button>
 </template>
 
 <script>
-  export default {}
+  export default {
+    props: ['icon', 'iconPosition']
+  }
 </script>
 
-<style lang="scss">
-  body {
-    background-color: red;
+<style lang="scss" scoped>
+  .g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 .8em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    /*内联样式会出现上下不对齐的奇怪问题，修改vertical-align就可以了*/
+    vertical-align: middle;
+
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+
+    &:focus {
+      outline: none;
+    }
+
+    &.icon-left,&.icon-undefined {
+      > .icon {
+        order: 1;
+        margin-right: .1em;
+      }
+      > .content{
+        order: 2;
+      }
+    }
+
+    &.icon-right {
+      > .icon {
+        order: 2;
+        margin-left: .1em;
+      }
+      > .content{
+        order: 1;
+      }
+    }
   }
 
-  .g-button {
-    border: 1px solid red;
-  }
 </style>
